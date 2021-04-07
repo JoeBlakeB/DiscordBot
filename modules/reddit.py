9e0e37b00cf3de23f453d069bdf4bb278457ec1e
JoeBaker <JoeBlakeB@gmail.com>
2021-04-07 16:07:28 +0100

    Hosted videos & reddit galleries

--- modules/reddit.py
+++ modules/reddit.py
@@ -1,12 +1,20 @@
+import aiohttp
 import asyncpraw, asyncprawcore
 import datetime
 import discord
-
+import json
 import time
 
 import baseClass
 from emojis import emojis
 import keys
+
+# TODO:
+# reddit: re route to subreddit, user, or url, fallback to r/all if none
+# subreddit
+# user
+# url: get id from url
+# postSubmission: support videos and galleries
 
 class reddit(baseClass.baseClass):
     try:
@@ -18,10 +26,28 @@ class reddit(baseClass.baseClass):
         prawInstance = Exception
         prawException = str(e)
 
+    recentPosts = {}
+    # {"ChannelID":{"DayOfYear":[PostIDs]}}
+    subredditCache = {}
+    # {"Subreddit":[0=Timestamp, 1:=PostIDs]}
+    cursedSubreddits = {}
+    # {"Subreddit":{"BanTime":Seconds, "NSFWOnlyBan":Bool "ContentWarning":"text", "BlockContent":("None"|NoMedia"|"Blocked Reason Text"|[ListOfImageURLs, #seperated])}}
+    # cursedSubreddits.txt: [subredditname], Ban[BanTime](NSFW| ), [BlockContent], CW:[ContentWarning, use \n](must be at end of line)
+    bannedUsers = {}
+    # {"UserID|ServerID":["BannedUntilTimestamp", NSFW:True]}
+
     async def reddit(self, message):
         return await message.add_reaction("❌")
 
     async def subreddit(self, message):
+        print(time.time())
+        subredditInstance = await self.prawInstance.subreddit("196")
+        print(time.time())
+        postIDs = []
+        async for post in subredditInstance.hot(limit=16):
+            postIDs += [post.id]
+        print(postIDs)
+        print(time.time())
         return await message.add_reaction("❌")
 
     async def user(self, message):
@@ -64,7 +90,7 @@ class reddit(baseClass.baseClass):
         if awardCount != 0:
             awards = "⠀" + emojis["RedditGold"] + "{0:,}".format(awardCount)
             if "--coin" in message.content.lower():
-                awards += "⠀" + emojis["Coin"] + " {0:,}".format(coinCount)
+                awards += "⠀" + emojis["Coin"] + "⠀{0:,}".format(coinCount)
         stats = emojis["Upvote"]+" {0:,}⠀🗨 {1:,}{2}\n> 🗓 {3}".format(submission.score, submission.num_comments, awards,
             datetime.datetime.fromtimestamp(submission.created_utc).strftime("%Y-%m-%d %H:%M"))
 
@@ -73,9 +99,14 @@ class reddit(baseClass.baseClass):
         if spoiler:
             spoilerLink = "||"
             spoilerText = "||"
+            if "?" in submission.url:
+                queryExtra = "&?"
+            else:
+                queryExtra = "?"
         else:
             spoilerLink = "> "
             spoilerText = ""
+            queryExtra = ""
 
         if submission.is_self and submission.selftext != "": # Text post
             if spoiler:
@@ -84,17 +115,53 @@ class reddit(baseClass.baseClass):
                 submissionData = "\n> " + submission.selftext.replace("\n", "\n> ")
             if len(submissionMetadata) + len(submissionData) > 2000:
                 submissionData = submissionData[:1956-len(submissionMetadata)] + spoilerText + "\n> (Discord max character limit reached)"
+        elif submission.url.startswith("https://www.reddit.com/gallery/"): # reddit galleries
+            try:
+                postJson = await self.postJson(f"https://www.reddit.com{submission.permalink}.json")
+                submissionData = ""
+                for item in postJson["gallery_data"]["items"]:
+                    try: previewURL = postJson["media_metadata"][item["media_id"]]["s"]["u"]
+                    except: previewURL = postJson["media_metadata"][item["media_id"]]["s"]["gif"]
+                    imageUrl = previewURL.replace("preview.redd.it", "i.redd.it").split("?")[0]
+                    submissionData += f"\n{spoilerLink}{imageUrl}{queryExtra}{spoilerText}"
+            except Exception as e:
+                e = e.replace("\n", "\n> ")
+                submissionData = f"\n> {e}"
+        elif submission.post_hint == "hosted:video": # Video post (not stuff like youtube)
+            try:
+                postJson = await self.postJson(f"https://www.reddit.com{submission.permalink}.json")
+                try:
+                    submissionData = postJson["secure_media"]["reddit_video"]["fallback_url"]
+                except:
+                    submissionData = postJson["preview"]["reddit_video_preview"]["fallback_url"]
+                if "?" in submissionData and spoiler: submissionData += "&?"
+                elif spoiler: submissionData += "?"
+                else:
+                    queryExtra = ""
+                    submissionData = submissionData.split("?source=fallback")[0]
+                submissionData = f"\n{spoilerLink}{submissionData}{spoilerText}"
+            except Exception as e:
+                e = e.replace("\n", "\n> ")
+                submissionData = f"\n> {e}\n{spoilerLink}{submission.url}{queryExtra}{spoilerText}"
         else: # Other links
             if submission.url != "https://www.reddit.com" + submission.permalink:
-                submissionData = f"\n{spoilerLink}{submission.url}{spoilerText}"
+                submissionData = f"\n{spoilerLink}{submission.url}{queryExtra}{spoilerText}"
             else:
                 submissionData = ""
-
-
 
         await message.channel.send(submissionMetadata + submissionData)
         print(str(message.author)+" NSFW:" + str(submission.over_18) +" "+ submission.subreddit.display_name+" "+ submission.id)
 
+    async def postJson(url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                if resp.status != 200:
+                    raise Exception("\n> HTTP error getting fallback URL: " + str(resp.status))
+                response = await resp.text()
+                postJson = json.loads(response)
+                # Get post data, crosspost data if it exists.
+                try: return postJson[0]["data"]["children"][0]["data"]["crosspost_parent_list"][0]
+                except: return postJson[0]["data"]["children"][0]["data"]
 
 reddit.mentionedCommands["reddit(?!\S)"] = [reddit.reddit, ["message"], {"self":reddit}]
 reddit.mentionedCommands["r\/([^\s\/]+)(?!\S)"] = [reddit.subreddit, ["message"], {"self":reddit}]
