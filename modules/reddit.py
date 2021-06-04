100604cf0f4d74a3bbbe73d51225f127f3739766
JoeBaker <JoeBlakeB@gmail.com>
2021-06-04 19:18:41 +0100

    praw is cringe, rewrote postSubmission

--- modules/reddit.py
+++ modules/reddit.py
@@ -1,5 +1,4 @@
 import aiohttp
-import asyncpraw, asyncprawcore
 import datetime
 import discord
 import json
@@ -11,15 +10,6 @@ from emojis import emojis
 import keys
 
 class reddit(baseClass.baseClass):
-    try:
-        secrets = keys.read("reddit-client_id", "reddit-client_secret", "reddit-username", "reddit-password")
-        prawInstance = asyncpraw.Reddit(user_agent="Joe#8648 - joeblakeb.github.io",
-            client_id=secrets[0], client_secret=secrets[1],
-            username=secrets[2], password=secrets[3])
-    except Exception as e:
-        prawInstance = Exception
-        prawException = str(e)
-
     recentPosts = {}
     # {"ChannelID":{"DayOfYear":[PostIDs]}}
     subredditCache = {}
@@ -78,36 +68,34 @@ class reddit(baseClass.baseClass):
             else:
                 submissionID = messageContentLower.split("/comments/")[1].split("/")[0]
             # get post from id
-            submission = await self.prawInstance.submission(submissionID)
+            submissionJson = await self.postJson(submissionID)
             try:
                 await message.edit(suppress=True)
             except: pass
-        except asyncprawcore.exceptions.NotFound:
+        except Exception as e:
+            await message.add_reaction("⚠️")
             try:
-                for emoji in "⚠️", "4️⃣", "0️⃣", emojis["Four"]:
-                    await message.add_reaction(emoji)
-                return
+                if e == "404":
+                    for emoji in "4️⃣", "0️⃣", emojis["Four"]:
+                        await message.add_reaction(emoji)
             except Exception: pass
-        await self.postSubmission(self, message, submission)
+        await self.postSubmission(self, message, submissionJson)
 
-    async def postSubmission(self, message, submission, crosspost=False, forceSpoiler=False):
-        title = "**" + submission.title + "**"
-        try:
-            author = "r/" + submission.subreddit.display_name + " • u/" + submission.author.name
-        except AttributeError:
-            author = "r/" + submission.subreddit.display_name + " • u/[DELETED]"
+    async def postSubmission(self, message, submissionJson, crosspost=False, forceSpoiler=False):
+        title = "**" + submissionJson["title"] + "**"
+        author = submissionJson["subreddit_name_prefixed"] + " • u/" + submissionJson["author"]
         spoiler = forceSpoiler
         extras = []
-        if submission.spoiler:
+        if submissionJson["spoiler"]:
             extras += ["**SPOILER**"]
             spoiler = True
             title = "⚠️ " + title
-        if submission.over_18:
+        if submissionJson["over_18"]:
             extras += ["**NSFW**"]
             spoiler = True
             title = "🔞 " + title
-        if submission.link_flair_text:
-            extras += ["({0})".format(submission.link_flair_text)]
+        if submissionJson["link_flair_text"]:
+            extras += ["({0})".format(submissionJson["link_flair_text"])]
         extras = " ".join(extras)
         if extras != "":
             extras = "\n> " + extras
@@ -115,17 +103,17 @@ class reddit(baseClass.baseClass):
         awards = ""
         awardCount = 0
         coinCount = 0
-        for award in submission.all_awardings:
+        for award in submissionJson["all_awardings"]:
             awardCount += award["count"]
             coinCount += award["count"] * award["coin_price"]
         if awardCount != 0:
             awards = "⠀" + emojis["RedditGold"] + "{0:,}".format(awardCount)
             if "--coin" in message.content.lower():
                 awards += "⠀" + emojis["Coin"] + "{0:,}".format(coinCount)
-        stats = emojis["Upvote"]+" {0:,}⠀🗨 {1:,}{2}\n> 🗓 {3}".format(submission.score, submission.num_comments, awards,
-            datetime.datetime.fromtimestamp(submission.created_utc).strftime("%Y-%m-%d %H:%M"))
+        stats = emojis["Upvote"]+" {0:,}⠀🗨 {1:,}{2}\n> 🗓 {3}".format(submissionJson["score"], submissionJson["num_comments"], awards,
+            datetime.datetime.fromtimestamp(submissionJson["created_utc"]).strftime("%Y-%m-%d %H:%M"))
 
-        submissionMetadata = f"> {title}\n> <https://redd.it/{submission.id}>\n> {author}{extras}\n> {stats}"
+        submissionMetadata = f"> {title}\n> <https://redd.it/{submissionJson['id']}>\n> {author}{extras}\n> {stats}"
 
         if spoiler:
             spoilerLink = "||"
@@ -134,26 +122,28 @@ class reddit(baseClass.baseClass):
             spoilerLink = "> "
             spoilerText = ""
 
-        if not hasattr(submission, "post_hint"):
-            submission.post_hint = "None"
+        try:
+            postHint = submissionJson["post_hint"]
+        except:
+            postHint = "None"
 
-        if submission.is_self and submission.selftext != "": # Text post
-            submissionBody = "\n> \n> " + submission.selftext.replace("\n", "\n> ")
-            if hasattr(submission, "poll_data"): # Poll post
-                pollLink = "https://www.reddit.com/poll/"+submission.id
+        if submissionJson["is_self"] and submissionJson["selftext"] != "": # Text post
+            submissionBody = "\n> \n> " + submissionJson["selftext"].replace("\n", "\n> ")
+            if True:
+                pollLink = "https://www.reddit.com/poll/"+submissionJson["id"]
                 submissionBody = submissionBody.replace(f"\n> \n> [View Poll]({pollLink})", "")
                 if submissionBody == "\n> \n> ": submissionBody = ""
                 pollData = "\n> "
-                open = datetime.datetime.utcnow().timestamp() < submission.poll_data.voting_end_timestamp/1000
+                open = datetime.datetime.utcnow().timestamp() < submissionJson["poll_data"]["voting_end_timestamp"]/1000
                 try:
                     options = {}
                     highestCount = 0
                     voteCounts = ""
-                    for option in submission.poll_data.options:
-                        options[option] = option.vote_count
-                        if option.vote_count > highestCount:
-                            highestCount = option.vote_count
-                    maxVoteLine = (submission.poll_data.total_vote_count*(.5-(len(options)/12))) + (highestCount*(.5+(len(options)/12)))
+                    for option in submissionJson["poll_data"]["options"]:
+                        options[option["text"]] = option["vote_count"]
+                        if option["vote_count"] > highestCount:
+                            highestCount = option["vote_count"]
+                    maxVoteLine = (submissionJson["poll_data"]["total_vote_count"]*(.5-(len(options)/12))) + (highestCount*(.5+(len(options)/12)))
                     for option in options:
                         voteCounts += f"\n> **{option}**: {options[option]}\n> "
                         voteBlocks = int((options[option] / maxVoteLine) * 240)
@@ -166,9 +156,9 @@ class reddit(baseClass.baseClass):
                     pollData += "\n> Error getting the option vote counts" + " as the vote has not closed yet"*int(open) + "."
                 else:
                     pollData += voteCounts
-                pollData += f"\n> Total Votes: {submission.poll_data.total_vote_count}"
-                pollData += "\n> " + pollLink + "\n> Voting " + ("closing" * int(open)) + ("closed" * int(not open)) + " at " + time.ctime(submission.poll_data.voting_end_timestamp/1000)
-            else: # Regular text post
+                pollData += f"\n> Total Votes: {submissionJson['poll_data']['total_vote_count']}"
+                pollData += "\n> " + pollLink + "\n> Voting " + ("closing" * int(open)) + ("closed" * int(not open)) + " at " + time.ctime(submissionJson["poll_data"]["voting_end_timestamp"]/1000)
+            else:
                 pollData = ""
 
             if spoiler:
@@ -179,25 +169,23 @@ class reddit(baseClass.baseClass):
                 submissionData = submissionData[:1956-(len(submissionMetadata)+len(pollData))] + spoilerText + "\n> (Discord max character limit reached)" + pollData
             else:
                 submissionData += pollData
-        elif submission.url.startswith("https://www.reddit.com/gallery/"): # reddit galleries
+        elif submissionJson["url"].startswith("https://www.reddit.com/gallery/"): # reddit galleries
             try:
-                postJson = await self.postJson(f"https://www.reddit.com{submission.permalink}.json")
                 submissionData = ""
-                for item in postJson["gallery_data"]["items"]:
-                    try: previewURL = postJson["media_metadata"][item["media_id"]]["s"]["u"]
-                    except: previewURL = postJson["media_metadata"][item["media_id"]]["s"]["gif"]
+                for item in submissionJson["gallery_data"]["items"]:
+                    try: previewURL = submissionJson["media_metadata"][item["media_id"]]["s"]["u"]
+                    except: previewURL = submissionJson["media_metadata"][item["media_id"]]["s"]["gif"]
                     imageUrl = previewURL.replace("preview.redd.it", "i.redd.it").split("?")[0]
                     submissionData += f"\n{spoilerLink}{imageUrl}{spoilerText}"
             except Exception as e:
                 e = e.replace("\n", "\n> ")
                 submissionData = f"\n> {e}"
-        elif submission.post_hint == "hosted:video": # Video post (not stuff like youtube)
+        elif postHint == "hosted:video": # Video post (not stuff like youtube)
             try:
-                postJson = await self.postJson(f"https://www.reddit.com{submission.permalink}.json")
                 try:
-                    submissionData = postJson["secure_media"]["reddit_video"]["fallback_url"]
+                    submissionData = submissionJson["secure_media"]["reddit_video"]["fallback_url"]
                 except:
-                    submissionData = postJson["preview"]["reddit_video_preview"]["fallback_url"]
+                    submissionData = submissionJson["preview"]["reddit_video_preview"]["fallback_url"]
                 if "?" in submissionData and spoiler: submissionData += "&?"
                 elif spoiler: submissionData += "?"
                 else:
@@ -205,13 +193,13 @@ class reddit(baseClass.baseClass):
                 submissionData = f"\n{spoilerLink}{submissionData}{spoilerText}"
             except Exception as e:
                 e = e.replace("\n", "\n> ")
-                submissionData = f"\n> {e}\n{spoilerLink}{submission.url}{spoilerText}"
-        elif submission.url == "https://www.reddit.com" + submission.permalink: # Nothing, should never be used
+                submissionData = f"\n> {e}\n{spoilerLink}{submissionJson['url']}{spoilerText}"
+        elif submissionJson["url"] == "https://www.reddit.com" + submissionJson["permalink"]: # Nothing, should never be used
             submissionData = ""
-        elif re.match("https:\/\/www.reddit.com\/r\/[^\s\/]+\/comments\/[^\s\/]+/", submission.url) and not crosspost: # Crossposts
+        elif re.match("https:\/\/www.reddit.com\/r\/[^\s\/]+\/comments\/[^\s\/]+/", submissionJson["url"]) and not crosspost: # Crossposts
             try:
-                submissionID = submission.url.split("/comments/")[1].split("/")[0]
-                submission = await self.prawInstance.submission(submissionID)
+                submissionID = submissionJson["url"].split("/comments/")[1].split("/")[0]
+                submission = await self.postJson(submissionID)
                 crosspostData = await self.postSubmission(self, message, submission, crosspost=True, forceSpoiler=spoiler)
                 crosspostLink = f"\n> \n> Crosspost: <https://redd.it/{submissionID}>\n"
                 if len(submissionMetadata) + len(crosspostData) + len(crosspostLink) > 2000:
@@ -219,38 +207,47 @@ class reddit(baseClass.baseClass):
                 else:
                     submissionData = crosspostLink + crosspostData
             except asyncprawcore.exceptions.NotFound:
-                submissionData = f"\n> \n> HTTP error 404 while getting crosspost.\n{spoilerLink}{submission.url}{spoilerText}"
+                submissionData = f"\n> \n> HTTP error 404 while getting crosspost.\n{spoilerLink}{submissionJson['url']}{spoilerText}"
         else: # Other links
-            submissionData = f"\n{spoilerLink}{submission.url}{spoilerText}"
+            submissionData = f"\n{spoilerLink}{submissionJson['url']}{spoilerText}"
 
         if crosspost:
             return submissionMetadata + submissionData
         else:
             await message.channel.send(submissionMetadata + submissionData)
-            print(str(message.author)+" NSFW:" + str(submission.over_18) +" "+ submission.subreddit.display_name+" "+ submission.id, flush=True)
+            print(str(message.author)+" NSFW:" + str(submissionJson["over_18"]) +" "+ submissionJson["subreddit_name_prefixed"]+" "+ submissionJson["id"], flush=True)
 
-    async def postJson(url):
+    async def postJson(submissionID):
+        url = "https://reddit.com/" + submissionID + ".json"
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 if resp.status != 200:
-                    raise Exception("\n> HTTP error getting fallback URL: " + str(resp.status))
+                    raise Exception(str(resp.status))
                 response = await resp.text()
                 postJson = json.loads(response)
-                # Get post data, crosspost data if it exists.
-                try: return postJson[0]["data"]["children"][0]["data"]["crosspost_parent_list"][0]
-                except: return postJson[0]["data"]["children"][0]["data"]
+                return postJson[0]["data"]["children"][0]["data"]
+
 
 # Use old post finder code till i have actually done the new one
 # legacyReddit.py (not commited) is reddit.py from before the rewrite
 # with only the prawInstance code removed
+# # Get post data, crosspost data if it exists.
+# try: return postJson[0]["data"]["children"][0]["data"]["crosspost_parent_list"][0]
+# except: return postJson[0]["data"]["children"][0]["data"]
 import bz2
 import os
-import json
 from legacyReddit import reddit as oldReddit
+import asyncpraw, asyncprawcore
 class legacyReddit(oldReddit):
-    postSubmission = reddit.postSubmission
-    postJson = reddit.postJson
-    prawInstance = reddit.prawInstance
+
+    try:
+        secrets = keys.read("reddit-client_id", "reddit-client_secret", "reddit-username", "reddit-password")
+        prawInstance = asyncpraw.Reddit(user_agent="Joe#8648 - joeblakeb.github.io",
+            client_id=secrets[0], client_secret=secrets[1],
+            username=secrets[2], password=secrets[3])
+    except Exception as e:
+        prawInstance = Exception
+        prawException = str(e)
     async def subreddit(self, message, commandContent):
         message.content = "joebot reddit " + commandContent[1:]
         command = message.content.split(" ")
@@ -266,11 +263,10 @@ reddit.mentionedCommands["reddit(?!\S)"] = [reddit.reddit, ["message"], {"self":
 reddit.mentionedCommands["r\/([^\s\/]+)(?!\S)"] = [reddit.subreddit, ["message", "commandContent"], {"self":reddit, "isSubreddit":True}]
 reddit.mentionedCommands["u\/[A-Za-z0-9_-]+(?!\S)"] = [reddit.subreddit, ["message", "commandContent"], {"self":reddit, "isSubreddit":False}]
 reddit.mentionedCommands["(http(s|):\/\/|)(www.|)redd(.it|it.com)\/"] = [reddit.url, ["message", "messageContentLower"], {"self":reddit, "exclamation":False}]
-reddit.exclamationCommands["reddit(?!\S)"] = [reddit.reddit, ["message"], {"self":reddit}]
 reddit.exclamationCommands["r\/([^\s\/]+)(?!\S)"] = [reddit.subreddit, ["message", "commandContent"], {"self":reddit, "isSubreddit":True}]
 reddit.exclamationCommands["u\/[A-Za-z0-9_-]+(?!\S)"] = [reddit.subreddit, ["message", "commandContent"], {"self":reddit, "isSubreddit":False}]
 reddit.exclamationCommands["(http(s|):\/\/|)(www.|)redd(.it|it.com)\/"] = [reddit.url, ["message", "messageContentLower"], {"self":reddit, "exclamation":True}]
-reddit.closeTasks += [reddit.prawInstance.close()]
+
 
 
 
@@ -281,3 +277,4 @@ reddit.exclamationCommands["!r\/([^\s\/]+)(?!\S)"] = [legacyReddit.subreddit, ["
 reddit.exclamationCommands["!u\/[A-Za-z0-9_-]+(?!\S)"] = [legacyReddit.user, ["message", "commandContent"], {"self":legacyReddit}]
 reddit.startTasks += [legacyReddit.loadRecentSubmissions(legacyReddit)]
 reddit.closeTasks += [legacyReddit.saveRecentSubmissions(legacyReddit)]
+reddit.closeTasks += [legacyReddit.prawInstance.close()]
