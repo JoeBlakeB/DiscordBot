7136c1d2d4201cabd701367a8483b56510f24bc7
Joe Baker <joeblakeb@gmail.com>
2022-10-08 10:03:37 +0100

    Remove username history faav.tk is down and mojang no longer supports username history so cannot get the username history any more

--- modules/minecraftUser.py
+++ modules/minecraftUser.py
@@ -32,7 +32,7 @@ class minecraftUser(baseClass.baseClass, baseClass.baseUtils):
         if len(username) > 16:
             profile = await self.getProfileFromUUID(self, username)
         else:
-            profile = await self.getProfileFromName(self, username)
+            profile = await self.getProfileFromUUID(self, username, url="https://api.mojang.com/users/profiles/minecraft/")
         embed = discord.Embed()
 
         if profile["name"][-1] == "s":
@@ -42,11 +42,8 @@ class minecraftUser(baseClass.baseClass, baseClass.baseUtils):
         if profile["name"].lower() == profile["name"]:
             titleAdd = titleAdd.lower()
         embed.title = profile["name"] + titleAdd
-
-        embed.description = f"**UUID**\n`{profile['id']}`\n`{profile['id'].replace('-', '')}`\n\n**Username History**"
-        for name in profile["history"]:
-            embed.description += "\n" + name
-        embed.description += f"\n\n**Skin:**\n[Skin File](https://crafatar.com/skins/{profile['id']}?overlay)\n[Head Render](https://crafatar.com/renders/head/{profile['id']}?overlay) - [(no layers)](https://crafatar.com/renders/head/{profile['id']})\n[Body Render](https://crafatar.com/renders/body/{profile['id']}?overlay) - [(no layers)](https://crafatar.com/renders/body/{profile['id']})"
+        
+        embed.description = f"**UUID**\n`{profile['id']}`\n`{profile['id'].replace('-', '')}`\n\n**Skin:**\n[Skin File](https://crafatar.com/skins/{profile['id']}?overlay)\n[Head Render](https://crafatar.com/renders/head/{profile['id']}?overlay) - [(no layers)](https://crafatar.com/renders/head/{profile['id']})\n[Body Render](https://crafatar.com/renders/body/{profile['id']}?overlay) - [(no layers)](https://crafatar.com/renders/body/{profile['id']})"
 
         embed.set_image(url="https://crafatar.com/renders/body/"+profile["id"]+"?overlay")
         embed.set_thumbnail(url="https://crafatar.com/avatars/"+profile["id"]+"?overlay")
@@ -60,48 +57,7 @@ class minecraftUser(baseClass.baseClass, baseClass.baseUtils):
         profile = mojangProfile[0]
         id = profile["id"]
         profile["id"] = f"{id[:8]}-{id[8:12]}-{id[12:16]}-{id[16:20]}-{id[20:]}"
-        profile["history"] = await self.getHistoryFromMojang(self, id)
         return profile
-
-    async def getProfileFromName(self, username):
-        try:
-            userProfile = await self.aiohttpGet("https://api.faav.tk/profiles/" + username, json=True)
-            profile = {"name":userProfile[0]["profiles"][0]["username"],
-                       "id": userProfile[0]["profiles"][0]["uuid"],
-                       "history": ["` Original Username ` " +
-                            userProfile[0]["profiles"][0]["username_history"][-1]["username"]]}
-            historyLength = 0
-            dateFormat = lambda date: (datetime.datetime.fromisoformat(date[:19])+datetime.timedelta(hours=3)).strftime("`%Y-%m-%d %H:%M:%S` ")
-            for name in userProfile[0]["profiles"][0]["username_history"][-2::-1]:
-                if historyLength > 3400:
-                    # incase someone has changed too much for discord text limit
-                    profile["history"].append(dateFormat(userProfile[0]["profiles"][0]["username_history"][0]["changed_at"]) +
-                        userProfile[0]["profiles"][0]["username_history"][0]["username"])
-                    break
-                else:
-                    profile["history"].append(dateFormat(name["changed_at"]) + name["username"])
-                    historyLength += 22 + len(name["username"])
-
-        except:
-            profile = await self.getProfileFromUUID(self, username, url="https://api.mojang.com/users/profiles/minecraft/")
-
-        return profile
-
-    async def getHistoryFromMojang(self, uuid):
-        response = await self.aiohttpGet("https://api.mojang.com/user/profiles/" + uuid + "/names", json=True)
-        names = ["` Original Username ` " +
-             response[0][0]["name"]]
-        historyLength = 0
-        dateFormat = lambda date : datetime.datetime.fromtimestamp(date/1000).strftime("`%Y-%m-%d %H:%M:%S` ")
-        for name in response[0][1:]:
-            if historyLength > 3400:
-                # incase someone has changed too much for discord text limit
-                names.append(dateFormat(response[0][-1]["changedToAt"]) + response[0][-1]["name"])
-                break
-            else:
-                names.append(dateFormat(name["changedToAt"]) + name["name"])
-                historyLength += 22 + len(name["name"])
-        return names
 
     async def getColor(self, url):
         response = await self.aiohttpGet(url, bytes=True)
